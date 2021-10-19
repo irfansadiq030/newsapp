c8f18a740d8435bdc567ce2d63b1cfdd221ef64e
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2021-10-19 15:44:55 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React Class Component</h1>
+      </div>
+    )
+  }
+}
+
